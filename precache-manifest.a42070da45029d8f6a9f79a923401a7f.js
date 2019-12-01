572a59cb0ea81dad7d92bbae1f5ff2fb3a454ff3
Mustafa Jebara <moosejebara@gmail.com>
2019-11-30 22:41:45 -0500

    Updates

---
+++ precache-manifest.a42070da45029d8f6a9f79a923401a7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6320f88324ce151e5a50c77cd525484",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6596efbf0cae679eeb0",
+    "url": "/static/css/main.354653eb.chunk.css"
+  },
+  {
+    "revision": "3f820f6668f0460ceeb7",
+    "url": "/static/js/2.e7c1ba77.chunk.js"
+  },
+  {
+    "revision": "c6596efbf0cae679eeb0",
+    "url": "/static/js/main.a16ffca5.chunk.js"
+  },
+  {
+    "revision": "78410a4da1c70cf4753e",
+    "url": "/static/js/runtime-main.4ff0c255.js"
+  }
+]);
